d454b192f48eb0212aad6613d756902ced052cde
Han Wu <wuhanstudios@gmail.com>
2026-05-15 11:26:58 +0100

    Update mnist_train_sd_card_4_bit.ino

--- platforms/arduino/examples/mnist_train_sd_card_4_bit/mnist_train_sd_card_4_bit.ino
+++ platforms/arduino/examples/mnist_train_sd_card_4_bit/mnist_train_sd_card_4_bit.ino
@@ -10,8 +10,8 @@
 
 static const char* TAG = "main";
 
-#define CONSOLE_USE_SERIAL
-// #define CONSOLE_USE_CDC
+// #define CONSOLE_USE_SERIAL
+#define CONSOLE_USE_CDC
 // #define CONSOLE_USE_RTT
 
 // Print to Serial 1
